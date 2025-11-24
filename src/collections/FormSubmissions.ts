6fcfb3a007d456e27b5bd541a233cda8fcfddf78
Omkar Dalvi <omkarsdalvi1@gmail.com>
2025-11-24 12:41:46 +0530

    Init blank payload template

---
+++ src/collections/FormSubmissions.ts
@@ -0,0 +1,32 @@
+import { CollectionConfig } from "payload";
+
+export const FormSubmissions: CollectionConfig = {
+    slug: "form-submissions",
+    fields: [
+        {
+            type: "text",
+            name: "from",
+            label: "From Email",
+            admin: {
+                readOnly: true,
+            }
+        },
+        {
+            type: "textarea",
+            name: "message",
+            label: "Message",
+            admin: {
+                readOnly: true,
+            }
+        },
+        {
+            type: "text",
+            name: "source",
+            label: "Source",
+            admin: {
+                position: "sidebar",
+                readOnly: true,
+            },
+        },
+    ],
+}
