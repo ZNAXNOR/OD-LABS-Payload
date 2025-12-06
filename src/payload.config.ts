962a2323408cfdd98731f7ace35e112d99741ccc
Omkar Dalvi <omkarsdalvi1@gmail.com>
2025-12-06 10:57:14 +0530

    Setup Email using Resend

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { resendAdapter } from '@payloadcms/email-resend'
 
 import sharp from 'sharp' // sharp-import
 import path from 'path'
@@ -57,6 +58,11 @@ export default buildConfig({
       ],
     },
   },
+  email: resendAdapter({
+    defaultFromAddress: 'contact@notifications.odtechlab.com',
+    defaultFromName: 'OD LABS',
+    apiKey: process.env.RESEND_API_KEY || '',
+  }),
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
   db: postgresAdapter({
