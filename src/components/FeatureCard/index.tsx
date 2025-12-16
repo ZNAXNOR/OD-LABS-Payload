f56ea4074373bc1e1ad8aa7fbb0c463b4f602b70
omkarsdalvi1 <omkarsdalvi1@gmail.com>
2025-12-16 05:09:57 +0000

    Update: payload latest on 16.12.2025

--- src/components/FeatureCard/index.tsx
+++ src/components/FeatureCard/index.tsx
@@ -7,8 +7,8 @@ export const FeatureCard: React.FC<{
 }> = ({ title, description }) => {
   return (
     <li className="group mt-10 first:mt-0">
-      <div className="pt-10 group-first:pt-0 group-first:before:hidden group-first:after:hidden relative before:absolute after:absolute before:bg-neutral-950 after:bg-neutral-950/10 before:top-0 before:left-0 before:h-px before:w-6 after:top-0 after:right-0 after:left-8 after:h-px">
-        <strong className="font-semibold text-neutral-950">{title}. </strong>
+      <div className="pt-10 group-first:pt-0 group-first:before:hidden group-first:after:hidden relative before:absolute after:absolute before:bg-od-brand-primary after:bg-border before:top-0 before:left-0 before:h-px before:w-6 after:top-0 after:right-0 after:left-8 after:h-px text-gray-700 dark:text-gray-300">
+        <strong className="font-semibold text-foreground">{title} </strong>
         {description}
       </div>
     </li>
